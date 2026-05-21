46928827a9c46ffbc35cbba4beaf31bcc372d7b2
Chin Tat Leong <142228496+chintatleong@users.noreply.github.com>
2026-05-21 18:17:07 +0100

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.py
@@ -0,0 +1,83 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+
+        # if len(rest) < k, leave the nodes
+
+        # reverse and then make A
+
+
+        # reverse and then make B
+        # Make A points to B (MUST)
+        # entire thing become A 
+        # reverse and then make new B
+        # Make A point B and become A (MUST)
+
+        dummy = ListNode()
+        tail = dummy
+
+        # initial
+        count = 0
+        prev = None
+        curr = head
+
+        start = curr
+
+        if not self.check_k(head, k):
+            return head
+            
+        while count < k:
+            count += 1
+            nxt = curr.next
+            curr.next = prev
+
+            prev = curr
+            curr = nxt
+
+        tail.next = prev
+        
+        # now prev is 3
+        while True:
+            if curr and (self.check_k(curr, k)):
+                count = 0
+                end = curr
+                prev = None
+
+                while count < k:
+                    count += 1
+
+                    if (curr == None):
+                        break
+                    nxt = curr.next
+                    curr.next = prev
+
+                    prev = curr
+                    curr = nxt
+
+                B = prev
+                start.next = B
+                start = end
+            else:
+                B = curr
+                start.next = B
+                break
+
+        
+            
+        return tail.next
+
+
+    def check_k(self, node, k):
+        for _ in range(k):
+            if not node:
+                return False
+
+            node = node.next
+
+        return True
