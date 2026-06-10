9fd4dc392f3831da7fdacf91a3087cddb1f9a473
Chin Tat Leong <142228496+chintatleong@users.noreply.github.com>
2026-06-10 17:01:47 +0100

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        res = []
+
+        def dfs(i, path):
+            if i == len(digits):
+                res.append(path)
+                return
+
+            for ch in phone[digits[i]]:
+                dfs(i+1, path+ch)    # i and path are copied, no need undo
+            
+        dfs(0, "")
+        return res
+                          
